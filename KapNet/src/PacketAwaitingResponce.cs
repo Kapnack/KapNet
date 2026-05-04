b3367ecf633bd1ca1d61ff44603ede6d00b8422d
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-05-04 17:54:07 -0300

    Init Repo

---
+++ KapNet/src/PacketAwaitingResponce.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+public class PacketAwaitingResponce
+{
+    public byte[] data;
+    public uint packetID;
+    public IPEndPoint ipEndPoint;
+    public DateTime lastTimeSent;
+
+    public PacketAwaitingResponce(byte[] data, uint packetID, IPEndPoint ipEndPoint, DateTime lastTimeSent)
+    {
+        this.data = data;
+        this.packetID = packetID;
+        this.ipEndPoint = ipEndPoint;
+        this.lastTimeSent = lastTimeSent;
+    }
+}
